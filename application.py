faa9463a9038e45e7db305f7cc1c0c1a9210b0f1
rakesh <rakeshcpr011@gmail.com>
2026-05-10 08:51:11 +0530

    a

--- application.py
+++ application.py
@@ -7,13 +7,12 @@ from src.mlproject.pipelines.prediction_pipeline import CustomData, PredictPipel
 application = Flask(__name__)
 app = application
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/predictdata', methods=['GET', 'POST'])
+@app.route('/predictscore', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
